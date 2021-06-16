46174a6bbd0aee49a7662e88ec0701a79c0ed901
Pooja Cheruku <poojacheruku@gmail.com>
2021-06-15 22:00:07 -0500

    changes for issue #1

--- src/auth/LiteratureAuth.cpp
+++ src/auth/LiteratureAuth.cpp
@@ -43,28 +43,28 @@ void LiteratureAuth::signIn()
       User* anonymous_user = anon_sign_in_for_user.result()
                                  ? *anon_sign_in_for_user.result()
                                  : nullptr;
-      if (anonymous_user != nullptr) {
-        LogMessage("Anonymous uid is %s", anonymous_user->uid().c_str());
-        ExpectStringsEqual("Anonymous user email", "",
-                           anonymous_user->email().c_str());
-        ExpectStringsEqual("Anonymous user display_name", "",
-                           anonymous_user->display_name().c_str());
-        ExpectStringsEqual("Anonymous user photo_url", "",
-                           anonymous_user->photo_url().c_str());
-        ExpectStringsEqual("Anonymous user provider_id", kFirebaseProviderId,
-                           anonymous_user->provider_id().c_str());
-        ExpectTrue("Anonymous user is_anonymous()",
-                   anonymous_user->is_anonymous());
-        ExpectFalse("Anonymous user is_email_verified()",
-                    anonymous_user->is_email_verified());
-        ExpectTrue("Anonymous user metadata().last_sign_in_timestamp != 0",
-                   anonymous_user->metadata().last_sign_in_timestamp != 0);
-        ExpectTrue("Anonymous user metadata().creation_timestamp != 0",
-                   anonymous_user->metadata().creation_timestamp != 0);
-      }
+    //   if (anonymous_user != nullptr) {
+    //     LogMessage("Anonymous uid is %s", anonymous_user->uid().c_str());
+    //     ExpectStringsEqual("Anonymous user email", "",
+    //                        anonymous_user->email().c_str());
+    //     ExpectStringsEqual("Anonymous user display_name", "",
+    //                        anonymous_user->display_name().c_str());
+    //     ExpectStringsEqual("Anonymous user photo_url", "",
+    //                        anonymous_user->photo_url().c_str());
+    //     ExpectStringsEqual("Anonymous user provider_id", kFirebaseProviderId,
+    //                        anonymous_user->provider_id().c_str());
+    //     ExpectTrue("Anonymous user is_anonymous()",
+    //                anonymous_user->is_anonymous());
+    //     ExpectFalse("Anonymous user is_email_verified()",
+    //                 anonymous_user->is_email_verified());
+    //     ExpectTrue("Anonymous user metadata().last_sign_in_timestamp != 0",
+    //                anonymous_user->metadata().last_sign_in_timestamp != 0);
+    //     ExpectTrue("Anonymous user metadata().creation_timestamp != 0",
+    //                anonymous_user->metadata().creation_timestamp != 0);
+    //   }
     }
-    cout << "\u2660" << "10" << endl;
-    cout << "\u2665" << "J" << endl;
-    cout << "\u2663" << "4" << endl;
-    cout << "\u2666" << "8" << endl;
+    // cout << "\u2660" << "10" << endl;
+    // cout << "\u2665" << "J" << endl;
+    // cout << "\u2663" << "4" << endl;
+    // cout << "\u2666" << "8" << endl;
 }
