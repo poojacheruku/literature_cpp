a6c1a277e22a3e5ad2ca754d17e36b76bc33bbb1
Pooja Cheruku <poojacheruku@gmail.com>
2021-06-22 18:28:07 -0500

    added game and player collection

--- src/desktop/desktop_main.cc
+++ src/desktop/desktop_main.cc
@@ -15,21 +15,133 @@
 #include "auth/LiteratureAuth.hpp"
 #include "utils.h"  // NOLINT
 #include <iostream>
+#include <vector>
+#include <random>
+#include "firebase/app.h"
+#include "firebase/firestore.h"
+using ::firebase::App;
+using ::firebase::firestore::Firestore;
+using ::firebase::firestore::DocumentReference;
+using ::firebase::Future;
+using ::firebase::firestore::FieldValue;
+using ::firebase::firestore::Error;
+using ::firebase::FutureHandleId;
+
 using namespace std;
 
+bool createGameReturned = false;
+bool gameCreated = false;
+string playerId;
+int gameCode;
+string displayName;
+
+bool waitForGameCreation()
+{
+  while(!createGameReturned) {
+    ProcessEvents(100);
+  }
+} 
+
+void createGame(int gameCode, string displayName, string playerId)
+{
+  cout << "Creating game..." << endl;
+
+  firebase::InitResult result;
+  Firestore* db = Firestore::GetInstance(LiteratureAuth::getInstance().getFirebaseApp());
+
+  // Add a new document with a generated ID
+  Future<DocumentReference> game_ref =
+    db->Collection("games").Add({
+            {"code", FieldValue::Integer(gameCode)},
+            {"status", FieldValue::String("Waiting for game be started...")},
+            {"playerId", FieldValue::String(playerId)}
+    });
+  
+  game_ref.OnCompletion([](const Future<DocumentReference>& future) {
+    if (future.error() == Error::kErrorOk) {
+      std::cout << "DocumentSnapshot added with ID: " << future.result()->id()
+                << '\n';
+      gameCreated = true;
+    } else {
+      std::cout << "Error adding document: " << future.error_message() << '\n';
+    }
+  });
+}
+
+void createPlayer(int code, string name)
+{
+  cout << "Creating player..." << endl;
+  gameCode = code;
+  displayName = name;
+
+  firebase::InitResult result;
+  Firestore* db = Firestore::GetInstance(LiteratureAuth::getInstance().getFirebaseApp());
+
+  // Add a new document with a generated ID
+  
+
+  Future<DocumentReference> player_ref = 
+    db->Collection("players").Add({{"displayName", FieldValue::String(displayName)}}); 
+                              
+
+  player_ref.OnCompletion([](const Future<DocumentReference>& future) {
+    if (future.error() == Error::kErrorOk) {
+      std::cout << "DocumentSnapshot added with ID: " << future.result()->id()
+                << '\n';
+      // gameCreated = true;
+      string playerId = future.result()->id();
+      createGame(gameCode, displayName, playerId);
+    } else {
+      std::cout << "Error adding document: " << future.error_message() << '\n';
+    }
+    createGameReturned = true;
+  });
+}
+
+
 int main(int argc, const char* argv[]) {
-  LiteratureAuth::getInstance().signIn();
+  LiteratureAuth::getInstance();
   cout << "1. Start a new game" << endl;
   cout << "2. Join a game" << endl ; 
   cout << endl; 
-  
+
   int choice; 
-  cout << "Choose your option (1 or 2): " << endl; 
+  cout << "Choose your option (1 or 2): "; 
   cin >> choice; 
+  cout << endl; 
 
-  // if (choice == 1)
-  // {
+  while (!(choice == 1) && !(choice == 2))
+  {
+    cout << "Please enter a valid option: "; 
+    cin >> choice; 
+  }
+  string displayName; 
+  cout << "Enter your name: "; 
+  cin >> displayName;
+  cout << endl; 
 
-  // }
+  if (choice == 1)
+  {
+    std::random_device rd;
+    std::default_random_engine rng(rd()); 
+    
+    std::vector<int> code(99999);
+    std::iota(code.begin(), code.end(), 00000);
+
+    shuffle(code.begin(), code.end(), rng);
+    int gameCode = code.back(); 
+    cout << "Your game code is: " << gameCode; 
+    code.pop_back();
+    cout << endl; 
+    createPlayer(gameCode, displayName);
+    waitForGameCreation();
+  }else if (choice == 2)
+  {
+    int code; 
+    cout << "Enter a 5 digit game code: "; 
+    cin >> code; 
+
+  }
+
   return 1;
 }
